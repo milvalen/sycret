1aa748aba311df9f4d1aed7c1d82942c64aafb8d
milvalen <artemyerkov@gmail.com>
2024-12-26 14:05:16 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import './assets/styles/reset.scss';
+import './assets/styles/globals.scss';
+import './assets/styles/style.scss';
+
+const App = () => <h1>Hello React</h1>;
+export default App;
