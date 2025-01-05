6b8eb11abdf605390efc0c5513fc82b4cab263c2
milvalen <artemyerkov@gmail.com>
2025-01-05 12:17:59 +0300

    format phone number

--- src/features/sale/ui.tsx
+++ src/features/sale/ui.tsx
@@ -14,6 +14,7 @@ export const Sale = () => {
   const dispatch = useDispatch<AppDispatch>();
 
   function handleChange(field: keyof typeof form, value: string | number) {
+    if (field.includes('Phone')) value = (value as string).replace(/\D/g, '');
     dispatch(saleSlice.actions.updateField({field, value: value}));
   }
 
